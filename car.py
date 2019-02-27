22db57190b2c576a8b0961e8c60821a76ad80b40
Jørgen <fugla-gutt@hotmail.com>
2019-02-27 09:07:37 +0100

    Add waittime for car

--- car.py
+++ car.py
@@ -3,13 +3,14 @@ from road import Road
 class Car:
     # A car needs to have an id, position and speed
 
-    def __init__(self, id: int, position: [float,float] = [0,0], speed: float = 0, road: Road = None, nextRoad: Road = None, carInFront: "Car" = None) -> None:
+    def __init__(self, id: int, position: [float,float] = [0,0], speed: float = 0, road: Road = None, nextRoad: Road = None, carInFront: "Car" = None, waitTime: float = 0) -> None:
         self.id = id
         self.position = position
         self.speed = speed
         self.road = road
         self.nextRoad = nextRoad
         self.carInFront = carInFront
+        self.waitTime = waitTime #Time that the car is standing still. Definition of still < 10 ?
 
 
     @property
@@ -54,11 +55,18 @@ class Car:
 
     @property
     def carInFront(self) -> "Car":
-        return self.carInFront
+        return self.__carInFront
 
     @carInFront.setter
     def carInFront(self, carInFront: "Car") -> None:
         self.__carInFront = carInFront
 
+    @property
+    def waitTime(self) -> float:
+        return self.__waitTime
+
+    @waitTime.setter
+    def waitTime(self, waitTime: float) -> None:
+        self.__waitTime = waitTime
 
     # TODO: We could add types of cars, and thus have pictures that match them. Size might also be needed
