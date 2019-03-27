96163ffdd8a981e6472f096e8347e616d7fb5f2f
Jørgen <fugla-gutt@hotmail.com>
2019-03-27 09:02:47 +0100

    Rewrite light class

--- light.py
+++ light.py
@@ -1,19 +1,12 @@
 from color import Color
 
-class Light:
 
+class Light:
     numberOfLights = 1
-    def __init__(self, green_max_time: int, green_countdown: int, color: Color = Color.RED) -> None:
+
+    def __init__(self, color: Color = Color.RED) -> None:
         self.id = Light.numberOfLights
         self.color = color
-        self.green_max_time = green_max_time
-        self.green_countdown = green_countdown
-
-    def incrementCountdown(self, increment) - > None:
-        self.__green_countdown += increment
-
-    def InitiateColorChange(self) -> None:
-        self.__color = Color.YELLOW
 
     @property
     def id(self) -> int:
@@ -30,22 +23,6 @@ class Light:
 
     @color.setter
     def color(self, color: Color) -> None:
-      if not isinstance(color, Color):
+        if not isinstance(color, Color):
             raise TypeError("Expected Color")
         self.__color = color
-
-    @property
-    def green_max_time(self) -> int:
-        return self.__green_max_time
-
-    @green_max_time.setter
-    def green_max_time(self, green_max_time: int) -> None:
-        self.__green_max_time = green_max_time
-
-    @property
-    def green_countdown(self) -> int:
-        return self.__green_countdown
-
-    @green_countdown.setter
-    def green_countdown(self, green_countdown: int) -> None:
-        self.__green_countdown = green_countdown
