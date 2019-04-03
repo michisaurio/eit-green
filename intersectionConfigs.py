b6d4343b890bd3cbe272553c1bd10c3f3d189c06
Qualdrion <ebogsnes@gmail.com>
2019-04-03 14:22:07 +0200

    Made lists 0-indexed

--- intersectionConfigs.py
+++ intersectionConfigs.py
@@ -1,17 +1,17 @@
 allowed = []
 
-initList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+initList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 
-notAllowed = [0, [4, 5, 6, 8, 9, 10, 13], [4, 6, 9, 12, 13, 14], [4, 16], [1, 2, 3, 7, 10, 13],
-              [8, 9, 10, 12, 13, 14, 1], [8, 10, 13, 16, 1, 2], [8, 4], [5, 6, 7, 11, 14, 1],
-              [12, 13, 14, 16, 1, 2, 5], [12, 14, 1, 4, 5, 6], [12, 8], [9, 10, 11, 15, 6, 5],
-              [16, 1, 2, 4, 5, 6, 9], [16, 2, 5, 8, 9, 10], [16, 12], [13, 14, 15, 3, 6, 9]]
+notAllowed = [[3, 4, 5, 7, 8, 9, 12], [3, 5, 8, 11, 12, 13], [3, 15], [0, 1, 2, 6, 9, 12],
+              [7, 8, 9, 11, 12, 13, 0], [7, 9, 12, 15, 0, 1], [7, 3], [4, 5, 6, 10, 13, 0],
+              [11, 12, 13, 15, 0, 1, 4], [11, 13, 0, 3, 4, 5], [11, 7], [8, 9, 10, 14, 1, 4],
+              [15, 0, 1, 3, 4, 5, 8], [15, 1, 4, 7, 8, 9], [15, 11], [12, 13, 14, 2, 5, 8]]
 
-softAllowed = [0, [4, 5, 6, 8, 9, 10, 11, 13, 14], [4, 5, 6, 9, 12, 13, 14, 15], [4, 6, 9, 16], [1, 2, 3, 7, 10, 13],
-               [2, 8, 9, 10, 12, 13, 14, 15, 1], [3, 8, 9, 10, 13, 16, 1, 2], [8, 4, 10, 13], [5, 6, 7, 11, 14, 1],
-               [3, 6, 12, 13, 14, 16, 1, 2, 5], [12, 14, 1, 4, 5, 6, 7, 13], [1, 12, 8, 14], [9, 10, 11, 15, 6, 5],
-               [16, 1, 2, 4, 5, 6, 9, 7, 10], [16, 2, 5, 8, 9, 10, 1, 11], [2, 5, 16, 12], [13, 14, 15, 3, 6, 9]]
+softAllowed = [[3, 4, 5, 7, 8, 9, 10, 12, 13], [3, 4, 5, 8, 11, 12, 13, 14], [3, 5, 8, 15], [0, 1, 2, 6, 9, 12],
+               [1, 7, 8, 9, 11, 12, 13, 14, 0], [2, 7, 8, 9, 12, 15, 0, 1], [7, 3, 9, 12], [4, 5, 6, 10, 13, 0],
+               [2, 5, 11, 12, 13, 15, 0, 1, 4], [11, 13, 0, 3, 4, 5, 6, 12], [0, 11, 7, 13], [8, 9, 10, 14, 1, 4],
+               [15, 0, 1, 3, 4, 5, 8, 6, 9], [15, 1, 4, 7, 8, 9, 0, 10], [1, 4, 15, 11], [12, 13, 14, 2, 5, 8]]
 
 
 # Modifies the restriction list (allowedList) by merging certain nodes such that for instance the light for moving
@@ -21,7 +21,7 @@ def merge(mergeList, allowedList):
     for i in range(len(mergeList)):
         for j in range(len(mergeList[i])):
             allowedList[mergeList[i][0]] = list(set().union(allowedList[mergeList[i][0]], allowedList[mergeList[i][j]]))
-            for k in range(1, len(allowedList)):
+            for k in range(0, len(allowedList)):
                 if mergeList[i][j] in allowedList[k]:
                     allowedList[k] = list(set().union(allowedList[k], mergeList[i]))
         for j in range(1, len(mergeList[i])):
@@ -33,7 +33,7 @@ def merge(mergeList, allowedList):
 # cars don't crash into each other, and softAllowed allows all configurations that don't crash into each other and
 # additionally requires that cars can't exit the intersection through the same direction from two different direction.
 def loop(curList, unused, num, allowedList):
-    if num != 0:
+    if num != -1:
         curList.append(num)
         for i in range(len(allowedList[num])):
                 if allowedList[num][i] in unused: unused.remove(allowedList[num][i])
@@ -49,6 +49,8 @@ def loop(curList, unused, num, allowedList):
 
 # Removes configurations that are a subset of some other configuration.
 def sublist(element, array):
+    if element == []:
+        return True
     for i in array:
         if len(element) >= len(i):
             continue
@@ -69,7 +71,7 @@ def main(mergeList=[], allowedList=notAllowed, deleteList=[]):
     merge(mergeList, allowedList)
     for i in deleteList:
         allowedList.remove(i)
-    loop([], initList, 0, allowedList)
+    loop([], initList, -1, allowedList)
     noSublistList = []
     for i in allowed:
         if not sublist(i, allowed):
@@ -78,4 +80,5 @@ def main(mergeList=[], allowedList=notAllowed, deleteList=[]):
 
 
 if __name__ == "__main__":
-    main(allowedList=notAllowed)
+    liste = main(mergeList=[[0, 1, 2], [4, 5, 6], [8, 9, 10], [12, 13, 14]])
+    print(liste)
