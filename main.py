8ed164435c069d6f9b07f44b6214e0fb4f8a4c42
Patsund <patrik2205@gmail.com>
2019-04-03 10:42:35 +0200

    Fixed pathing of lane11

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from light import Light
 import color
 #TODO Tune parameters
 def main():
-    lane1 = Lane(coordinates=[0,375,300,375], speedLimit=120, curveType="line", spawnRate=0)
+    lane1 = Lane(coordinates=[0,375,300,375], speedLimit=120, curveType="line", spawnRate=0.5)
     lane2 = Lane(coordinates = [300,425,0,425], speedLimit = 120, curveType="line")
-    lane3 = Lane(coordinates=[375, 800, 375, 500], speedLimit=120, curveType="line", spawnRate=0.5)
+    lane3 = Lane(coordinates=[375, 800, 375, 500], speedLimit=120, curveType="line", spawnRate=0)
     lane4 = Lane(coordinates=[425, 500, 425, 800], speedLimit=120, curveType="line")
     lane5 = Lane(coordinates=[800,425,425,500], speedLimit=120, curveType="line", spawnRate=0)
     lane6 = Lane(coordinates=[500,375,800,375], speedLimit=120, curveType="line")
     lane7 = Lane(coordinates=[425, 0, 425, 300], speedLimit=120, curveType="line", spawnRate=0)
     lane8 = Lane(coordinates=[375, 300, 375, 0], speedLimit=120, curveType="line")
-    lane11 = Lane(coordinates=[300, 375, 375, 500], speedLimit=120, curveType="ellipsis", isClockWise=0, nextLanes=[(lane4, 1)])
+    lane11 = Lane(coordinates=[300, 375, 425, 500], speedLimit=120, curveType="ellipsis", isClockWise=0, nextLanes=[(lane4, 1)])
     lane12 = Lane(coordinates=[300, 375, 500, 375], speedLimit=120, curveType="line", nextLanes=[(lane6, 1)])
     lane13 = Lane(coordinates=[300, 375, 375, 300], speedLimit=120, curveType="ellipsis", isClockWise=1, nextLanes=[(lane8, 1)])
     lane31 = Lane(coordinates=[375, 500, 500, 375], speedLimit=120, curveType="ellipsis", isClockWise=0 ,nextLanes=[(lane6, 1)])
@@ -26,7 +26,7 @@ def main():
     lane71 = Lane(coordinates=[425, 300, 300, 425], speedLimit=120, curveType="ellipsis", isClockWise=0, nextLanes=[(lane2, 1)])
     lane72 = Lane(coordinates=[425, 300, 425, 500], speedLimit=120, curveType="line", nextLanes=[(lane4, 1)])
     lane73 = Lane(coordinates=[425, 300, 500, 375], speedLimit=120, curveType="ellipsis", isClockWise=1, nextLanes=[(lane6, 1)])
-    lane1.nextLanes = [(lane11, 0.2), (lane12, 0.6), (lane13, 1)]
+    lane1.nextLanes = [(lane11, 1), (lane12, 0), (lane13, 0)] #TODO: fix this
     lane3.nextLanes = [(lane31, 0.2), (lane32, 0.6), (lane33, 1)]
     lane5.nextLanes = [(lane51, 0.2), (lane52, 0.6), (lane53, 1)]
     lane7.nextLanes = [(lane71, 0.2), (lane72, 0.6), (lane73, 1)]
